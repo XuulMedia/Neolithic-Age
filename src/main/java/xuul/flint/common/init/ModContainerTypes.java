0ce2d36a6b66041de231afe2ccc6789d361b0858
Elenterius <elenterius@users.noreply.github.com>
2022-02-12 18:41:58 +0100

    fixes basket item inventory and makes the basket dye-able

--- src/main/java/xuul/flint/common/init/ModContainerTypes.java
+++ src/main/java/xuul/flint/common/init/ModContainerTypes.java
@@ -1,15 +1,22 @@
 package xuul.flint.common.init;
 
-import xuul.flint.Flint;
-import xuul.flint.common.gui.BasketContainer;
-import xuul.flint.common.gui.FlintStationContainer;
+import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.world.inventory.MenuType;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.extensions.IForgeMenuType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import xuul.flint.Flint;
+import xuul.flint.client.BasketScreen;
+import xuul.flint.client.FlintStationScreen;
+import xuul.flint.common.gui.BasketMenu;
+import xuul.flint.common.gui.FlintStationContainer;
 
-public class ModContainerTypes {
+public final class ModContainerTypes {
+
+	private ModContainerTypes() {}
 
     public static final DeferredRegister<MenuType<?>> CONTAINERS = DeferredRegister.create(ForgeRegistries.CONTAINERS, Flint.MOD_ID);
 
@@ -17,7 +24,12 @@ public class ModContainerTypes {
     public static final RegistryObject<MenuType<FlintStationContainer>> FLINT_STATION_CONTAINER = CONTAINERS.register("flint_station",
             () -> IForgeMenuType.create((windowId, inv, data) -> new FlintStationContainer(windowId, data.readBlockPos(), inv, inv.player)));
 
-    public static final RegistryObject<MenuType<BasketContainer>> BASKET_CONTAINER = CONTAINERS.register("basket",
-            () -> IForgeMenuType.create((windowId, inv, data) -> new BasketContainer(windowId, inv)));
+    public static final RegistryObject<MenuType<BasketMenu>> BASKET_CONTAINER = CONTAINERS.register("basket", () -> IForgeMenuType.create(BasketMenu::createClientMenu));
+
+	@OnlyIn(Dist.CLIENT)
+	public static void registerMenuScreens() {
+		MenuScreens.register(ModContainerTypes.FLINT_STATION_CONTAINER.get(), FlintStationScreen::new);
+		MenuScreens.register(ModContainerTypes.BASKET_CONTAINER.get(), BasketScreen::new);
+	}
 
 }
